a45d1bb14bef1cb3061d2ace5b9eb7ab2b16f86a
Kosta.Kiryazov <Kosta.Kiryazov@bmsys.eu>
2017-12-14 10:49:10 +0200

    Initialization of the solution

---
+++ Development/CSharpBasic/01.Introduction-to-Programming/04.HelloWorld/PrintHelloWorld.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file=PrintHelloWorld company="Business Management Systems Ltd">
+//   Copyright (c) 2017 All Rights Reserved
+// </copyright>
+// <author>Kosta Kiryazov</author>
+// <date>12/14/2017 10:44:19 AM</date>
+// <summary>Class representing a PrintHelloWorld entity</summary>
+
+namespace _04.HelloWorld
+{
+    #region Usings
+    using System;
+
+    #endregion
+
+    public class PrintHelloWorld
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, C#!");
+        }
+    }
+}
