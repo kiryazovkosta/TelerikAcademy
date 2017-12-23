2bf5ddfc6e742e59cc86cb38a3d21759a90d2da4
kosta.kiryazov@gmail.com <kosta.kiryazov@gmail.com>
2017-12-23 08:48:04 +0200

    Homework: Data Types and Variables - part 1

---
+++ Development/CSharpBasic/02. Data-Types-and-Variables/01.DeclareVariables/Variables.cs
@@ -0,0 +1,23 @@
+﻿/// <summary>
+/// Declare five variables choosing for each of them the most appropriate of the types byte, sbyte, short, 
+/// ushort, int, uint, long, ulong to represent the following values: 52130, -115, 4825932, 97, -10000. 
+/// Choose a large enough type for each number to ensure it will fit in it. Try to compile the code.
+/// </summary>
+namespace _01.DeclareVariables
+{
+    using System;
+
+    public class Variables
+    {
+        public static void Main()
+        {
+            ushort us = 52130;
+            sbyte b = -115;
+            int i = 4825932;
+            byte b2 = 97;
+            short s = -10000;
+
+            Console.WriteLine($"{us} {b} {i} {b2} {s}");
+        }
+    }
+}
