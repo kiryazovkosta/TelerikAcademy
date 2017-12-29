7aa17a210bbb909a917a607ba453b9e23be42be6
Kosta.Kiryazov <Kosta.Kiryazov@bmsys.eu>
2017-12-29 16:15:28 +0200

    Task from Strings homework - part three

---
+++ Development/CSharpAdvanced/05. Strings-and-Text-Processing/09.ForbiddenWords/ForbiddenWords.cs
@@ -0,0 +1,15 @@
+﻿namespace _09.ForbiddenWords
+{
+    using System;
+
+    /// <summary>
+    /// We are given a string containing a list of forbidden words and a text containing some of these words. 
+    /// Write a program that replaces the forbidden words with asterisks.
+    /// </summary>
+    public class ForbiddenWords
+    {
+        public static void Main()
+        {
+        }
+    }
+}
