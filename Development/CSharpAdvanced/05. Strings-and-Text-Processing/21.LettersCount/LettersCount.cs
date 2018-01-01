f98c90748a4cb76cc036d53ee7aa3dd6c23e8b9b
kosta.kiryazov@gmail.com <kosta.kiryazov@gmail.com>
2018-01-01 23:32:05 +0200

    strings

---
+++ Development/CSharpAdvanced/05. Strings-and-Text-Processing/21.LettersCount/LettersCount.cs
@@ -0,0 +1,42 @@
+﻿namespace _21.LettersCount
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Write a program that reads a string from the console and 
+    /// prints all different letters in the string along with information how many times each letter is found.
+    /// </summary>
+    public class LettersCount
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine().ToLower();
+            char[] separators = text.Where(c => !char.IsLetter(c)).Distinct().ToArray();
+
+            var lettersCount = new Dictionary<char, int>();
+            foreach (var letter in text)
+            {
+                if (!char.IsLetter(letter))
+                {
+                    continue;
+                }
+
+                if (lettersCount.ContainsKey(letter))
+                {
+                    lettersCount[letter]++;
+                }
+                else
+                {
+                    lettersCount[letter] = 1;
+                }
+            }
+
+            foreach (var record in lettersCount)
+            {
+                Console.WriteLine($"{record.Key} -> {record.Value}");
+            }
+        }
+    }
+}
---
+++ Development/CSharpAdvanced/05. Strings-and-Text-Processing/22.WordsCount/WordsCount.cs
@@ -0,0 +1,38 @@
+﻿namespace _22.WordsCount
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Write a program that reads a string from the console and 
+    /// lists all different words in the string along with information how many times each word is found.
+    /// </summary>
+    public class WordsCount
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine();
+            char[] separators = text.Where(c => !char.IsLetter(c)).Distinct().ToArray();
+            var words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            var wordsCount = new Dictionary<string, int>();
+            foreach (var word in words)
+            {
+                if (wordsCount.ContainsKey(word))
+                {
+                    wordsCount[word]++;
+                }
+                else
+                {
+                    wordsCount[word] = 1;
+                }
+            }
+
+            foreach (var record in wordsCount)
+            {
+                Console.WriteLine($"{record.Key} -> {record.Value}");
+            }
+        }
+    }
+}
