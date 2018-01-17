ac11e6486ef8b410efc465966b54ca5251d70bec
kosta.kiryazov@gmail.com <kosta.kiryazov@gmail.com>
2018-01-18 00:12:45 +0200

    LINQ and Lambda Expressions

---
+++ Development/Object-Oriented-Programming/03.ExtensionDelegatesLambdaLINQ/09.StudentGroups/StudentGroups.cs
@@ -0,0 +1,112 @@
+﻿namespace _09.StudentGroups
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Utility;
+
+    public class StudentGroups
+    {
+        private const int GroupNumber = 2;
+
+        private const string Email = "abv.bg";
+
+        public static void Main()
+        {
+            // Create a List<Student> with sample students.
+            var students = Student.Students;
+
+            //  Select only the students that are from group number 2. Use LINQ query. Order the students by FirstName.
+            var selectedStudents = GetStudentsFromGroupLinq(students, GroupNumber);
+            selectedStudents.Each(Console.WriteLine);
+            Console.WriteLine();
+
+            //  Select only the students that are from group number 2. Use extension methods. Order the students by FirstName.
+            selectedStudents = GetStudentsFromGroupEM(students, GroupNumber);
+            selectedStudents.Each(Console.WriteLine);
+            Console.WriteLine();
+
+            // Extract all students that have email in abv.bg. Use string methods and LINQ.
+            selectedStudents = GetStudentsByEmail(students, Email);
+            selectedStudents.Each(Console.WriteLine);
+            Console.WriteLine();
+
+            // Extract all students with phones in Sofia. Use LINQ.
+            selectedStudents = ExtractStudentsByPhoneInSofia(students);
+            selectedStudents.Each(Console.WriteLine);
+            Console.WriteLine();
+
+            // elect all students that have at least one mark Excellent (6) into a new anonymous class that has properties – FullName and Marks. Use LINQ.
+            var excelentStudents =
+                from student in students
+                where student.Marks.Contains(6)
+                select new
+                {
+                    FullName = $"{student.FirstName} {student.LastName}",
+                    student.Marks
+                };
+            foreach (var item in excelentStudents)
+            {
+                Console.WriteLine($"{item.FullName} -> {string.Join(", ", item.Marks)}");
+            }
+
+            Console.WriteLine();
+
+            // Write down a similar program that extracts the students with exactly two marks "2". Use extension methods.
+            var weakStudents = students
+                .Where(x => x.Marks.FindAll(s => s == 2).Count() == 2)
+                .Select(s => new
+                {
+                    FullName = $"{s.FirstName} {s.LastName}",
+                    s.Marks
+                });
+            foreach (var item in weakStudents)
+            {
+                Console.WriteLine($"{item.FullName} -> {string.Join(", ", item.Marks)}");
+            }
+
+            Console.WriteLine();
+        }
+
+        public static IEnumerable<Student> GetStudentsFromGroupLinq(IEnumerable<Student> students, int groupNumber)
+        {
+            var orderedStudents =
+                from student in students
+                where student.GroupNumber == groupNumber
+                orderby student.FirstName
+                select student;
+            return orderedStudents;
+        }
+
+        public static IEnumerable<Student> GetStudentsFromGroupEM(IEnumerable<Student> students, int groupNumber)
+        {
+            var orderedStudents = students
+                .Where(x => x.GroupNumber == groupNumber)
+                .OrderBy(x => x.FirstName)
+                .ToList();
+            return orderedStudents;
+        }
+
+        public static IEnumerable<Student> GetStudentsByEmail(IEnumerable<Student> students, string email)
+        {
+            var selectedStudents =
+                from student in students
+                where student.Email.ToLower().EndsWith(email.ToLower())
+                select student;
+
+            return selectedStudents;
+        }
+
+        public static IEnumerable<Student> ExtractStudentsByPhoneInSofia(IEnumerable<Student> students)
+        {
+            var selectedStudents =
+                from student in students
+                where student.Telephone.StartsWith("+3592") || student.Telephone.StartsWith("02")
+                select student;
+
+            return selectedStudents;
+        }
+    }
+}
--- Development/Object-Oriented-Programming/03.ExtensionDelegatesLambdaLINQ/Utility/Student.cs
+++ Development/Object-Oriented-Programming/03.ExtensionDelegatesLambdaLINQ/Utility/Student.cs
@@ -1,29 +1,178 @@
 ﻿namespace Utility
 {
+    using System;
     using System.Collections.Generic;
 
     public class Student
     {
         private static List<Student> students = new List<Student>()
         {
-            new Student { FirstName = "Ivan", LastName = "Popov", Age = 16 },
-            new Student { FirstName = "Kiril", LastName = "Angelov", Age = 22},
-            new Student { FirstName = "Bilqn", LastName = "Nenchev", Age = 27},
-            new Student { FirstName = "Dimitar", LastName = "Donkov", Age = 14},
-            new Student { FirstName = "Asen", LastName = "Kolchev", Age = 32},
-            new Student { FirstName = "Deyan", LastName = "Zhelev", Age = 11},
-            new Student { FirstName = "Todor", LastName = "Milchev", Age = 5},
-            new Student { FirstName = "Vasil", LastName = "Borisov", Age = 24},
-            new Student { FirstName = "Trayan", LastName = "Jelev", Age = 13},
-            new Student { FirstName = "Krasimir", LastName = "Lechev", Age = 17},
-            new Student { FirstName = "Lazar", LastName = "Uzunov", Age = 29},
-            new Student { FirstName = "Georgi", LastName = "Georgiev", Age = 18},
-            new Student { FirstName = "Krasimir", LastName = "Ivanov", Age = 13},
+            new Student { FirstName = "Ivan", LastName = "Popov", Age = 16 , FaculteteNumber = 201801010001, Email="ivan@yahoo.com", Telephone="02896512345", GroupNumber = 1, Marks = new List<int>(){ 2, 4, 5, 3, 2, 6, 3 } },
+            new Student { FirstName = "Kiril", LastName = "Angelov", Age = 22, FaculteteNumber = 201801010002, Email="kiril@abv.bg", Telephone="+359296512345", GroupNumber = 1, Marks = new List<int>(){ 4, 2, 5, 3, 4, 6, 3 } },
+            new Student { FirstName = "Bilqn", LastName = "Nenchev", Age = 27, FaculteteNumber = 201801010003, Email="ivan@mail.bg", Telephone="05696512345", GroupNumber = 1, Marks = new List<int>(){ 4, 4, 5, 3, 4, 6, 3 } },
+            new Student { FirstName = "Dimitar", LastName = "Donkov", Age = 14, FaculteteNumber = 201801010004, Email="ivan@mail.bg", Telephone="05296512345", GroupNumber = 2, Marks = new List<int>(){ 6, 6, 5, 6, 6, 6, 6 } },
+            new Student { FirstName = "Asen", LastName = "Kolchev", Age = 32, FaculteteNumber = 201801010005, Email="ivan@yahoo.com", Telephone="+35996512345", GroupNumber = 2, Marks = new List<int>(){ 4, 4, 2, 3, 4, 4, 3 } },
+            new Student { FirstName = "Deyan", LastName = "Zhelev", Age = 11, FaculteteNumber = 201801010006, Email="ivan@post.bg", Telephone="026512345", GroupNumber = 2, Marks = new List<int>(){ 4, 4, 5, 3, 4, 5, 3 } },
+            new Student { FirstName = "Todor", LastName = "Milchev", Age = 5, FaculteteNumber = 201801010007, Email="todor@abv.bg", Telephone="+359512345", GroupNumber = 2, Marks = new List<int>(){ 4, 4, 5, 3, 4, 6, 3 } },
+            new Student { FirstName = "Vasil", LastName = "Borisov", Age = 24, FaculteteNumber = 201801010008, Email="ivan@yahoo.com", Telephone="03496512345", GroupNumber = 3, Marks = new List<int>(){ 4, 4, 5, 3, 4, 6, 3 } },
+            new Student { FirstName = "Trayan", LastName = "Jelev", Age = 13, FaculteteNumber = 201801010009, Email="ivan@yahoo.com", Telephone="0551196512345", GroupNumber = 3, Marks = new List<int>(){ 4, 4, 5, 3, 4, 5, 3 } },
+            new Student { FirstName = "Krasimir", LastName = "Lechev", Age = 17, FaculteteNumber = 201801010010, Email="ivan@yahoo.com", Telephone="044321112", GroupNumber = 3, Marks = new List<int>(){ 4, 4, 5, 3, 4, 6, 3 } },
+            new Student { FirstName = "Lazar", LastName = "Uzunov", Age = 29, FaculteteNumber = 201801010011, Email="lazar@abv.bg", Telephone="+3596512345", GroupNumber = 3, Marks = new List<int>(){ 4, 2, 5, 3, 4, 3, 2 } },
+            new Student { FirstName = "Georgi", LastName = "Georgiev", Age = 18, FaculteteNumber = 201801010012, Email="ivan@google.com", Telephone="0896512345", GroupNumber = 4, Marks = new List<int>(){ 2, 2, 2, 2, 2, 2, 2 } },
+            new Student { FirstName = "Krasimir", LastName = "Ivanov", Age = 13, FaculteteNumber = 201801010013, Email="ivan@google.com.com", Telephone="+35923446", GroupNumber = 4, Marks = new List<int>(){ 6, 6, 6, 6, 6, 6, 6 } },
         };
 
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public int Age { get; set; }
+        private string firstName;
+        private string lastName;
+        private int age;
+        private long faculteteNumber;
+        private string telephone;
+        private string email;
+        private List<int> marks = new List<int>();
+        private int groupNumber;
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 3)
+                {
+                    throw new ArgumentException(nameof(this.firstName));
+                }
+
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 3)
+                {
+                    throw new ArgumentException(nameof(this.lastName));
+                }
+
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(nameof(this.age));
+                }
+
+                this.age = value;
+            }
+        }
+
+        public long FaculteteNumber
+        {
+            get
+            {
+                return this.faculteteNumber;
+            }
+            set
+            {
+                this.faculteteNumber = value;
+            }
+        }
+
+        public string Telephone
+        {
+            get
+            {
+                return this.telephone;
+            }
+            set
+            {
+                if (value.Length < 5)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(this.telephone));
+                }
+                if (!value.StartsWith("+359") && !value.StartsWith("0"))
+                {
+                    throw new ArgumentException(nameof(this.telephone));
+                }
+
+                this.telephone = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return this.email;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(this.email));
+                }
+
+                if (!value.Contains(".") || !value.Contains("@"))
+                {
+                    throw new ArgumentException(nameof(this.email));
+                }
+
+                this.email = value;
+            }
+        }
+
+        public List<int> Marks
+        {
+            get
+            {
+                return this.marks;
+            }
+            set
+            {
+                foreach (var item in value)
+                {
+                    if (item <= 1 || item >= 7)
+                    {
+                        throw new ArgumentException(nameof(this.marks));
+                    }
+                }
+
+                this.marks = value;
+            }
+        }
+
+        public int GroupNumber
+        {
+            get
+            {
+                return this.groupNumber;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(nameof(this.groupNumber));
+                }
+
+                this.groupNumber = value;
+            }
+        }
 
         public static List<Student> Students
         {
@@ -35,7 +184,7 @@
 
         public override string ToString()
         {
-            return $"{this.FirstName} {this.LastName} {this.Age}";
+            return $"{this.FirstName} {this.LastName} {this.Age} {this.GroupNumber} {this.Email} {this.Telephone}";
         }
     }
 }
